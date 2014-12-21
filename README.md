8f0548098f934030df1d79646724f5727612c94b
kaposztastomi <kaposztastomi@gmail.com>
2014-12-21 22:35:16 +0100

    work

---
+++ README.md
@@ -0,0 +1,17 @@
+---
+title: "ReadMe"
+
+---
+
+This document describes contents required for Course Project for Getting and Cleaning Data Course.
+
+The initial datasets cointain data collected by wearable smart devices, splitted to train and test set.
+
+
+#Files
+
+run_analysis.R: R script reads the required files, merge them and select the appropriate colums.
+
+CodeBook.md: Describes transformations performed in the script.
+
+averages_data.txt: contains tidy dataset required in step5.
---
+++ run_analysis.R
@@ -0,0 +1,56 @@
+rm(list=ls(all=TRUE))  # remove all variable
+library(dplyr)
+
+#WorkingDirectory <- 'c:/Coursera/R/DataCleaning/Week2'   # set WD
+
+setwd('c:/Coursera/R/DataCleaning/Week 2/')
+
+
+## READ files
+
+feature = read.table("./UCI HAR Dataset/features.txt")
+
+
+trainid = read.table("./UCI HAR Dataset/train/subject_train.txt", col.name="Subject")
+trainx = read.table("./UCI HAR Dataset/train/X_train.txt", col.names=feature$V2)
+trainy = read.table("./UCI HAR Dataset/train/y_train.txt", col.name="Code")
+
+
+
+label =  read.table("./UCI HAR Dataset/activity_labels.txt",col.name=c("Code","Activity"))
+
+
+testid = read.table("./UCI HAR Dataset/test/subject_test.txt", col.name="Subject")
+testx = read.table("./UCI HAR Dataset/test/X_test.txt", col.names=feature$V2)
+testy =  read.table("./UCI HAR Dataset/test/y_test.txt",col.names="Code")
+
+
+
+
+testmergex <- cbind(testid, testx)
+testmergey <- merge(testy, label, by="Code")
+
+
+trainmergex <- cbind(trainid, trainx)
+trainmergey<-merge(trainy, label, by="Code")
+
+FullTestDataSet<-cbind(testmergex,testmergey)
+FullTrainDataSet<-cbind(trainmergex,trainmergey)
+
+View(FullTestDataSet)
+View(FullTrainDataSet)
+
+
+FullDataSet <- rbind(FullTrainDataSet,FullTestDataSet)
+
+
+cols<-grep("mean",feature$V2)
+cols<-append(cols,grep("Mean",names(FullDataSet)))
+cols<-append(cols,grep("std",names(FullDataSet)))
+cols<-append(cols,grep("Code",names(FullDataSet)))
+cols<-append(cols,grep("Subject",names(FullDataSet)))
+cols<-append(cols,grep("Activity",names(FullDataSet)))
+
+
+selected<-select(FullDataSet,cols)
+
